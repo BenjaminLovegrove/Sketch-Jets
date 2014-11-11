7e1650b568f6b286166f398653857d2ecdfdd2ce
BenjaminLovegrove <Benjamin_jl@Live.com>
2014-11-11 19:39:48 +1000

    Gravity changes
    Gravity and planet size tweaks

---
+++ Sketch Jets/Assets/Scripts/CameraControllerBasic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControllerBasic : MonoBehaviour {
+
+	public GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Get player position and move camera to it
+		//Will change this to be p1 - p2 for inbetween once p2 is implemented
+		Vector3 cameraMove = new Vector3 (players [0].transform.position.x, players [0].transform.position.y, transform.position.z);
+
+		transform.position = cameraMove;
+	}
+}
