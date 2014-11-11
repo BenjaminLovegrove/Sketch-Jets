3b53a524d8c723bc5103d53b348b22429cf972a5
BenjaminLovegrove <Benjamin_jl@Live.com>
2014-11-11 19:16:05 +1000

    Planets and gravity added!!!
    Placeholder planets and gravity implemented finally =D

---
+++ Sketch Jets/Assets/Scripts/PlanetGravity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravity : MonoBehaviour {
+
+	public float gravPull;
+	public Vector2 direction;
+
+	void OnTriggerStay2D (Collider2D col) {
+
+		gravPull = Vector3.Distance (transform.position, col.transform.position);
+		direction = transform.position - col.transform.position;
+
+		col.rigidbody2D.AddForce (direction * 8 / gravPull);
+	}
+}
