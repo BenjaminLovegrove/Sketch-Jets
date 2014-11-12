f3d2df5f64956365bd5a66cc3fc9f251b11a96c9
Stewman13 <110367@student.sae.edu.au>
2014-11-13 02:18:59 +1000

    Changed Player1&2 Code
    Neatened up a lot of the player scripts, and made tags for pickups

--- Sketch Jets/Assets/Scripts/P1Controller.cs
+++ Sketch Jets/Assets/Scripts/P1Controller.cs
@@ -7,7 +7,6 @@ public class P1Controller : MonoBehaviour {
 public float movSpd = 15.0f;
 public float angle;
 public float fireSpeed = 0.0f;
-public int numEquip = 1;
 public bool rocketPickedUp = false;
 public bool laserPickedUp = false;
 public bool gaussPickedUp = false;
@@ -23,51 +22,65 @@ public GameObject primaryWeapon;
 
 
 
-void Update () {
-		
-	Vector3 moveDir = new Vector3 ();
-		
-	moveDir.x = Input.GetAxis ("Horizontal1");
-	moveDir.y = Input.GetAxis ("Vertical1");
-		
-	this.rigidbody2D.AddForce (moveDir.normalized * movSpd);
+	void Update () {
 
-	float x = Input.GetAxis("RotHorizontal1");
-	float y = Input.GetAxis("RotVertical1");
-	
- 	if (x != 0.0f || y != 0.0f) {
-		angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
-		transform.eulerAngles = new Vector3(0, 0, angle);
+		Movement();
+		Fire();
+		Pickups();
+		SwitchWeps();
 	}
 
 
-	//WEAPON FIRE SECTION!!!
+	void Movement (){
 
+		Vector3 moveDir = new Vector3 ();
+		
+		moveDir.x = Input.GetAxis ("Horizontal1");
+		moveDir.y = Input.GetAxis ("Vertical1");
+		
+		this.rigidbody2D.AddForce (moveDir.normalized * movSpd);
+		
+		float x = Input.GetAxis("RotHorizontal1");
+		float y = Input.GetAxis("RotVertical1");
+		
+		if (x != 0.0f || y != 0.0f) {
+			angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0, 0, angle);
+		}
+	}
+
+
+	void Fire(){
 
 		//Fires Rockets
-	if (Input.GetButtonDown("Fire1") && primaryWeapon == rocket) {
+		if (Input.GetButtonDown("Fire1") && primaryWeapon == rocket) {
 			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
 		}
 		//Fires Machineguns
-	if (Input.GetButtonDown("Fire1") && primaryWeapon == machineGun) {
+		if (Input.GetButtonDown("Fire1") && primaryWeapon == machineGun) {
 			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
 		}
 		//Fires Laser
-	if (Input.GetButton("Fire1") && primaryWeapon == laser) {
+		if (Input.GetButton("Fire1") && primaryWeapon == laser) {
 			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
 		}
-	if (Input.GetButtonDown("Fire1") && primaryWeapon == gauss) {
+		if (Input.GetButtonDown("Fire1") && primaryWeapon == gauss) {
 			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
 		}
+	}
 
 
-	//WEAPON EQUIP SECTION!!!
+	void Pickups(){
+		
+	}
 
+
+	void SwitchWeps(){
 
 		//Equips Rockets
-	if (primaryWeapon == machineGun && rocketPickedUp == true && Input.GetButtonDown ("Switch")) {
-		primaryWeapon = rocket;
-		print ("Rocket equiped");
+		if (primaryWeapon == machineGun && rocketPickedUp == true && Input.GetButtonDown ("Switch")) {
+			primaryWeapon = rocket;
+			print ("Rocket equiped");
 		}
 		//Equips Lasers from Machineguns
 		else if (primaryWeapon == machineGun && rocketPickedUp == false && laserPickedUp == true && Input.GetButtonDown ("Switch")) {
@@ -79,11 +92,11 @@ void Update () {
 			primaryWeapon = gauss;
 			print ("Gauss equiped");
 		}
-
+		
 		//Machineguns from Rockets
-	else if (primaryWeapon == rocket && laserPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch")) {
-		primaryWeapon = machineGun;
-		print ("Machinegun equiped");
+		else if (primaryWeapon == rocket && laserPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch")) {
+			primaryWeapon = machineGun;
+			print ("Machinegun equiped");
 		}
 		//Lasers from Rockets
 		else if (primaryWeapon == rocket && laserPickedUp == true && Input.GetButtonDown ("Switch")) {
@@ -95,11 +108,11 @@ void Update () {
 			primaryWeapon = gauss;
 			print ("Gauss equiped");
 		}
-
+		
 		//Machinegun from Lasers
-	else if (primaryWeapon == laser && rocketPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch")) {
-		primaryWeapon = machineGun;
-		print ("Machinegun equiped");	
+		else if (primaryWeapon == laser && rocketPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch")) {
+			primaryWeapon = machineGun;
+			print ("Machinegun equiped");	
 		}
 		//Rocket from Lasers
 		else if (primaryWeapon == laser && gaussPickedUp == false && rocketPickedUp == true && Input.GetButtonDown ("Switch")) {
@@ -111,9 +124,9 @@ void Update () {
 			primaryWeapon = gauss;
 			print ("Gauss equiped");
 		}
-
+		
 		//Machinegun from Gauss
-	else if (primaryWeapon == gauss && Input.GetButtonDown ("Switch")) {
+		else if (primaryWeapon == gauss && Input.GetButtonDown ("Switch")) {
 			primaryWeapon = machineGun;
 			print ("Machinegun equiped");	
 		}
--- Sketch Jets/Assets/Scripts/P2Controller.cs
+++ Sketch Jets/Assets/Scripts/P2Controller.cs
@@ -5,45 +5,139 @@ public class P2Controller : MonoBehaviour {
 	
 	
 public float movSpd = 15.0f;
-public GameObject bullet2;
 public float angle;
 public float fireSpeed = 0.0f;
+public bool rocketPickedUp = false;
+public bool laserPickedUp = false;
+public bool gaussPickedUp = false;
+
+	//Attached Game Objects
 public GameObject bltSpn;
+public GameObject machineGun;
 public GameObject rocket;
+public GameObject laser;	
+public GameObject gauss;
 public GameObject primaryWeapon;
-public int numEquip = 1;
 
 
 
-void Update () {
-		
-	Vector3 moveDir = new Vector3 ();
-		
-	moveDir.x = Input.GetAxis ("Horizontal2");
-	moveDir.y = Input.GetAxis ("Vertical2");
-		
-	this.rigidbody2D.AddForce (moveDir.normalized * movSpd);
 
-	float x = Input.GetAxis("RotHorizontal2");
-	float y = Input.GetAxis("RotVertical2");
-		
-	if (x != 0.0f || y != 0.0f) {
-		angle = Mathf.Atan2 (y, x) * Mathf.Rad2Deg;
-		transform.eulerAngles = new Vector3(0, 0, angle);
+	void Update () {
+
+		Movement();
+		Fire();
+		Pickups();
+		SwitchWeps();
+
 	}
 
-		//fires
-	if (Input.GetButtonDown ("Fire2")) {
+
+	void Movement (){
+
+		Vector3 moveDir = new Vector3 ();
+		
+		moveDir.x = Input.GetAxis ("Horizontal2");
+		moveDir.y = Input.GetAxis ("Vertical2");
+		
+		this.rigidbody2D.AddForce (moveDir.normalized * movSpd);
+		
+		float x = Input.GetAxis("RotHorizontal2");
+		float y = Input.GetAxis("RotVertical2");
+		
+		if (x != 0.0f || y != 0.0f) {
+			angle = Mathf.Atan2 (y, x) * Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0, 0, angle);
+		}
+	}
+
+
+	void Fire(){
+		//Fires Rockets
+		if (Input.GetButtonDown("Fire2") && primaryWeapon == rocket) {
 			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
 		}
-	if (primaryWeapon == bullet2 && Input.GetButtonDown ("Switch2")) {
-		primaryWeapon = rocket;
-		print ("rocket equiped");
-		} 
+		//Fires Machineguns
+		if (Input.GetButtonDown("Fire2") && primaryWeapon == machineGun) {
+			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
+		}
+		//Fires Laser
+		if (Input.GetButton("Fire2") && primaryWeapon == laser) {
+			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
+		}
+		if (Input.GetButtonDown("Fire2") && primaryWeapon == gauss) {
+			Instantiate (primaryWeapon, bltSpn.transform.position, bltSpn.transform.rotation);
+		}
+	}
 
-	else if (primaryWeapon == rocket && Input.GetButtonDown ("Switch2")) {
-		primaryWeapon = bullet2;
-		print ("bullet equiped");
+
+	void Pickups(){
+
+	}
+
+
+	void SwitchWeps(){
+		//Equips Rockets
+		if (primaryWeapon == machineGun && rocketPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = rocket;
+			print ("Rocket equiped");
+		}
+		//Equips Lasers from Machineguns
+		else if (primaryWeapon == machineGun && rocketPickedUp == false && laserPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = laser;
+			print ("Laser equiped");
+		}
+		//Gauss from Machineguns
+		else if (primaryWeapon == machineGun && rocketPickedUp == false && laserPickedUp == false && gaussPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = gauss;
+			print ("Gauss equiped");
+		}
+		
+		//Machineguns from Rockets
+		else if (primaryWeapon == rocket && laserPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = machineGun;
+			print ("Machinegun equiped");
+		}
+		//Lasers from Rockets
+		else if (primaryWeapon == rocket && laserPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = laser;
+			print ("Laser equiped");
+		}
+		//Gauss from Rockets
+		else if (primaryWeapon == rocket && laserPickedUp == false && gaussPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = gauss;
+			print ("Gauss equiped");
+		}
+		
+		//Machinegun from Lasers
+		else if (primaryWeapon == laser && rocketPickedUp == false && gaussPickedUp == false && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = machineGun;
+			print ("Machinegun equiped");	
+		}
+		//Rocket from Lasers
+		else if (primaryWeapon == laser && gaussPickedUp == false && rocketPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = rocket;
+			print ("Rocket equiped");
+		}
+		//Gauss from Lasers
+		else if (primaryWeapon == laser && gaussPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = gauss;
+			print ("Gauss equiped");
+		}
+		
+		//Machinegun from Gauss
+		else if (primaryWeapon == gauss && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = machineGun;
+			print ("Machinegun equiped");	
+		}
+		//Rocket from Gauss
+		else if (primaryWeapon == gauss && rocketPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = rocket;
+			print ("Rocket equiped");	
+		}
+		//Laser from Gauss
+		else if (primaryWeapon == gauss && rocketPickedUp == false && laserPickedUp == true && Input.GetButtonDown ("Switch2")) {
+			primaryWeapon = laser;
+			print ("Laser equiped");	
 		}
 	}
 }
