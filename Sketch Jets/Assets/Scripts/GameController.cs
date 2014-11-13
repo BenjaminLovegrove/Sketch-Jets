8e825788941848d97646ceb7bebf7d543e6844e3
Stewman13 <110367@student.sae.edu.au>
2014-11-13 17:29:17 +1000

    Destroyable Enemy Doppler
    The doppler enemy is now detroyable, he has 50 hp and each weapon deals
    a different amount of damage to it

---
+++ Sketch Jets/Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public float P1Health = 100f;
+	public float P2Health = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Sketch Jets/Assets/Scripts/S_EnemyBeh_Doppler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_EnemyBeh_Doppler : MonoBehaviour {
+
+
+	public float Health = 50;
+	public float Damage = 5;
+
+
+	void Update () {
+		HP();
+	}
+
+	void HP (){
+		if (Health <= 0){
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag ==  "RocketRound"){
+			Health -= 25;
+			Destroy (col.gameObject);
+		}
+		if (col.gameObject.tag ==  "LaserBeam"){
+			Health -= 2;
+			Destroy (col.gameObject);
+		}
+		if (col.gameObject.tag ==  "GaussRound"){
+			Health -= 100;
+			Destroy (col.gameObject);
+		}
+		if (col.gameObject.tag ==  "MachinegunRound"){
+			Health -= 5;
+			Destroy (col.gameObject);
+		}
+	}
+}
