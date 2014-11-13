3b4ed70f7f5817431e1d480bafe198bff729ec37
Stewman13 <110367@student.sae.edu.au>
2014-11-14 03:37:26 +1000

    Added Simple Laser Action
    as it says

--- Sketch Jets/Assets/Scripts/S_Wep_Laser.cs
+++ Sketch Jets/Assets/Scripts/S_Wep_Laser.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class S_Wep_Laser : MonoBehaviour {
 
+
+	public float bltSpd = 20;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		Destroy (this.gameObject, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.rigidbody2D.AddForce (this.transform.up * rigidbody2D.mass * bltSpd / Time.fixedDeltaTime);
 	}
 }
