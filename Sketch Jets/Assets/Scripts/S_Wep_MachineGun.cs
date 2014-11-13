180bf12d0156e9ae28dadf316fc279fbecf45c10
Stewman13 <110367@student.sae.edu.au>
2014-11-14 03:23:28 +1000

    Rockets Move
    added a rocket movement scrpt

--- Sketch Jets/Assets/Scripts/S_Wep_MachineGun.cs
+++ Sketch Jets/Assets/Scripts/S_Wep_MachineGun.cs
@@ -8,5 +8,6 @@ public class S_Wep_MachineGun : MonoBehaviour {
 	// Update is called once per frame
 	void Start () {
 		this.rigidbody2D.AddForce (this.transform.up * rigidbody2D.mass * bltSpd / Time.fixedDeltaTime);
+		Destroy (this.gameObject, 2);
 	}
 }
--- Sketch Jets/Assets/Scripts/S_Wep_Rocket.cs
+++ Sketch Jets/Assets/Scripts/S_Wep_Rocket.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class S_Wep_Rocket : MonoBehaviour {
 
+
+	public float bltSpd = 20;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		Destroy (this.gameObject, 3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.rigidbody2D.AddForce (this.transform.up * rigidbody2D.mass * bltSpd);
 	}
 }
