e82b73fe64fd3df71ada370c02a1da4dc1762c2f
Stewman13 <110367@student.sae.edu.au>
2014-11-16 19:23:24 +1000

    Cleaned up Script

--- Sketch Jets/Assets/Scripts/CameraControllerBasic.cs
+++ Sketch Jets/Assets/Scripts/CameraControllerBasic.cs
@@ -14,19 +14,8 @@ public class CameraControllerBasic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		//Get player position and move camera to it
-		//Will change this to be p1 - p2 for inbetween once p2 is implemented
-		//if (players [1] != null){
-		//	Vector3 cameraMove = new Vector3 ((players [0].transform.position.x, players [0].transform.position.y, transform.position.z) - (players [1].transform.position.x, players [1].transform.position.y, transform.position.z));
-		//	transform.position = cameraMove;
-		//} else {
-
+		
 		Vector3 cameraMove = ((players [1].transform.position - players [0].transform.position)/2.0f) + players [0].transform.position + Zaxis;
 		transform.position = cameraMove;
-
-		//}
-
-
 	}
 }
