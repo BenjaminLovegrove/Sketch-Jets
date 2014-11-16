61eef77f2214788a8eff4fbc74488cd1a204a39f
Stewman13 <110367@student.sae.edu.au>
2014-11-17 02:57:37 +1000

    Health Bars
    Got a really simple GUI based health bar done, However! this way seems
    to be unefficient and needs to be fixed. part of the reason as to why
    it's bad is because I'm having diffuculty placing P2 health bar in the
    right spot. I will try to find a better way to do this tomorrow

--- Sketch Jets/Assets/Scripts/GameController.cs
+++ Sketch Jets/Assets/Scripts/GameController.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public int P1maxHealth = 100;
+	public int P2maxHealth = 100;
 	public float P1Health = 100f;
 	public float P2Health = 100f;
 	public GameObject Player1;
 	public GameObject Player2;
 	public bool P1needRevive = false;
 	public bool P2needRevive = false;
+	public Texture2D fgImage; 
+	public float P1healthBarLength;
+	public float P2healthBarLength;
 
 	// Use this for initialization
 	void Start () {
-	
+		P1healthBarLength = 300;  
+		P2healthBarLength = 300; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		AddjustCurrentHealth(0);
 		Death();
 	}
 
@@ -36,5 +43,63 @@ public class GameController : MonoBehaviour {
 	}
 	void P2TenDamage(){
 		P2Health -= 10;
+	}
+
+
+	void OnGUI () {
+
+		// Create one Group to contain both images
+		// Adjust the first 2 coordinates to place it somewhere else on-screen
+		GUI.BeginGroup (new Rect (0,0, P1healthBarLength,32));
+		
+		// Create a second Group which will be clipped
+		// We want to clip the image and not scale it, which is why we need the second Group
+		GUI.BeginGroup (new Rect (0,0, P1Health / P1maxHealth * P1healthBarLength, 32));
+		
+		// Draw the foreground image
+		GUI.Box (new Rect (0,0,P1healthBarLength,32), fgImage);
+		
+		// End both Groups
+		GUI.EndGroup ();
+		
+		GUI.EndGroup ();
+
+
+		// Create one Group to contain both images
+		// Adjust the first 2 coordinates to place it somewhere else on-screen
+		GUI.BeginGroup (new Rect (0,0, P2healthBarLength,32));
+		
+		// Create a second Group which will be clipped
+		// We want to clip the image and not scale it, which is why we need the second Group
+		GUI.BeginGroup (new Rect (0,0, P2Health / P2maxHealth * P2healthBarLength, 32));
+		
+		// Draw the foreground image
+		GUI.Box (new Rect (0,0,P2healthBarLength,32), fgImage);
+		
+		// End both Groups
+		GUI.EndGroup ();
+		
+		GUI.EndGroup ();
+	}
+
+	public void AddjustCurrentHealth(int adj){
+		
+		P1Health += adj;
+		P2Health += adj;
+
+		if(P1Health < 0){
+			P1Health = 0;
+		}
+		if(P2Health < 0){
+			P2Health = 0;
+		}
+		if(P1Health > P1maxHealth){
+			P1Health = P1maxHealth;
+		}
+		if(P2Health > P2maxHealth){
+			P2Health = P2maxHealth;
+		}
+		P1healthBarLength = 300 * (P1Health / (float)P1maxHealth);
+		P2healthBarLength = 300 * (P2Health / (float)P2maxHealth);
 	}
 }
