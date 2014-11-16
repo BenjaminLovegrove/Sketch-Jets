d7523b9d135cb8767d69ae4df1df969284bf060b
Stewman13 <110367@student.sae.edu.au>
2014-11-16 19:37:49 +1000

    Player Pull
    if player 1 and 2 get too far apart they will be pulled back together so
    that they can always be seen

--- Sketch Jets/Assets/Scripts/CameraControllerBasic.cs
+++ Sketch Jets/Assets/Scripts/CameraControllerBasic.cs
@@ -5,6 +5,7 @@ public class CameraControllerBasic : MonoBehaviour {
 
 	public Vector3 Zaxis;
 	public GameObject[] players;
+	public Vector2 direction;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +14,26 @@ public class CameraControllerBasic : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		CenterCam();
+		PullPlayers();
+	}
+
+	void CenterCam()
+	{
 		Vector3 cameraMove = ((players [1].transform.position - players [0].transform.position)/2.0f) + players [0].transform.position + Zaxis;
 		transform.position = cameraMove;
+	}
+
+	void PullPlayers()
+	{
+		direction = players[0].transform.position - players[1].transform.position;
+
+		if (Vector3.Distance (players[0].transform.position, players[1].transform.position) > 17)
+		{
+			players[0].rigidbody2D.AddForce (direction * -5);
+			players[1].rigidbody2D.AddForce (direction * 5);
+		}
 	}
 }
