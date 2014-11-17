7a86d4fa35ae63a25901a23b2aa0c27f92b12a1a
BenjaminLovegrove <Benjamin_jl@Live.com>
2014-11-18 04:22:42 +1000

    Planet capture script

---
+++ Sketch Jets/Assets/Scripts/S_PlanetCapture_PH.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_PlanetCapture_PH : MonoBehaviour {
+	
+	public SpriteRenderer renderer;
+	public float captureTimer = 5;
+
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer> ();
+		renderer.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider col) {
+		captureTimer -= Time.deltaTime;
+
+		if (captureTimer < 0){
+			renderer.color = Color.green;
+		}
+	}
+
+	void OnTriggerExit (Collider col) {
+		captureTimer = 5;
+	}
+}
