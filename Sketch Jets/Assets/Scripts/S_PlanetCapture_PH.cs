9cc407e70d5a0a95c8b539f733364417ac7a9acb
BenjaminLovegrove <Benjamin_jl@Live.com>
2014-11-18 05:47:13 +1000

    Planet capture script started

--- Sketch Jets/Assets/Scripts/S_PlanetCapture_PH.cs
+++ Sketch Jets/Assets/Scripts/S_PlanetCapture_PH.cs
@@ -3,22 +3,21 @@ using System.Collections;
 
 public class S_PlanetCapture_PH : MonoBehaviour {
 	
-	public SpriteRenderer renderer;
+	public SpriteRenderer texRenderer;
 	public float captureTimer = 5;
 
 
 	// Use this for initialization
 	void Start () {
-		renderer = GetComponent<SpriteRenderer> ();
-		renderer.color = Color.red;
+		texRenderer = GetComponent<SpriteRenderer> ();
+		texRenderer.color = Color.red;
 	}
-	
-	// Update is called once per frame
+
 	void OnTriggerStay (Collider col) {
 		captureTimer -= Time.deltaTime;
 
 		if (captureTimer < 0){
-			renderer.color = Color.green;
+			texRenderer.color = Color.green;
 		}
 	}
 
