1a7fe46400fa32aef2743e8dc430fad94f7c4452
BenjaminLovegrove <Benjamin_jl@Live.com>
2014-12-02 02:24:14 +1000

    Boss warning text size fix.

--- Sketch Jets/Assets/Scripts/S_Hud.cs
+++ Sketch Jets/Assets/Scripts/S_Hud.cs
@@ -44,7 +44,7 @@ public class S_Hud : MonoBehaviour {
 		GUI.Label (new Rect (Screen.width / 2 - 70, Screen.height * 0.95f, 50, 50), "Planets Left: " + planetsLeft.ToString (), guiStyle01); 
 		GUI.Label (new Rect (Screen.width / 2 - 70, Screen.height * 0.90f, 50, 50), "Score: " + Score.ToString (), guiStyle01); 
 		if (boss){
-			GUI.Label (new Rect (Screen.width / 2 - 70, Screen.height * 0.85f, 50, 50), "Boss Spawned!"); 
+			GUI.Label (new Rect (Screen.width / 2 - 70, Screen.height * 0.85f, 50, 50), "Boss Spawned!", guiStyle01); 
 		}
 	}
 
