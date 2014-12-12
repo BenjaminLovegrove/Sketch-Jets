0eff37663957d0ed177cf9904f2498903a1fda99
Stewman13 <110367@student.sae.edu.au>
2014-12-12 19:18:42 +1000

    Fixed 'To Many Planet' bug
    added a new script just for the minimap planets

---
+++ Sketch Jets/Assets/Scripts/S_PlanetMinimapIcon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_PlanetMinimapIcon : MonoBehaviour {
+
+	public SpriteRenderer texRenderer;
+	public float captureTimer = 15;
+	public bool captured = false;
+	
+	
+	// Use this for initialization
+	void Start () {
+		texRenderer = GetComponent<SpriteRenderer> ();
+		texRenderer.color = Color.red;
+	}
+	
+	void OnTriggerStay2D (Collider2D col) {
+		if (col.gameObject.tag == "Player"){
+			captureTimer -= Time.deltaTime;
+		}
+		
+		if (captureTimer < 0 && captured == false){
+			texRenderer.color = Color.green;
+			captured = true;
+		}
+	}
+}
